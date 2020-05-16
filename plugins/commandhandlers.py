a0da2d5a851340cb0cbc09cc26fc34c269a875e6
alenpaul2001 <alenpaul2001@gmail.com>
2020-05-16 23:38:31 +0530

    initial commit

---
+++ plugins/commandhandlers.py
@@ -0,0 +1,72 @@
+from pyrogram import Client,Filters,InlineKeyboardButton,InlineKeyboardMarkup
+import asyncio
+import os 
+
+@Client.on_message(Filters.command(["start"]))
+async def start(client,message):
+    await client.send_message(
+        chat_id=message.chat.id,
+        text='Choose',
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text='decrypt',callback_data='decrypter')]
+        ])
+    )
+
+
+
+@Client.on_message(Filters.document)
+async def downloader(client,message):
+    if message.document.mime_type == "application/pdf":
+        #Download and storing file
+        location = "./FILES" + "/" + str(message.chat.id)
+        imgdir =  location + "/" + "input.pdf"
+        if not os.path.isdir(location):
+            os.mkdir(location)
+        dwn = await message.reply_text("Downloading...", quote=True)
+        await client.download_media(
+            message=message,
+            file_name=imgdir
+        )
+        await dwn.edit(
+        text='Choose any options',
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text='Compress',callback_data='compress')],
+            [InlineKeyboardButton(text='Split and Merge',callback_data='s&m')],
+            [InlineKeyboardButton(text='PDF Protections',callback_data='pass')]
+            
+        ])
+    )
+    else:
+        await message.reply(text='Oops This is not a pdf')
+
+
+@Client.on_callback_query()
+async def cb_(client,callback_query):
+    cb_data = callback_query.data 
+    msg = callback_query.message
+    if cb_data == 's&m':
+        await msg.edit(
+            text='Please Choose',
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton(text='Split PDF',callback_data='slicer')],
+                [InlineKeyboardButton(text='Merge PDF',callback_data='merger')]
+            ])
+        )
+    elif cb_data == 'pass':
+        await msg.edit(
+            text='Please Choose',
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton(text='encrypt',callback_data='encrypter')],
+                [InlineKeyboardButton(text='decrypt',callback_data='decrypter')]
+            ])
+        )
+    elif cb_data == 'compress':
+        await msg.edit(
+            text='Please Select Compresssion Ratio',
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton(text='low',callback_data='low | compress')],
+                [InlineKeyboardButton(text='recommended',callback_data='medium | compress')],
+                [InlineKeyboardButton(text='high',callback_data='high | compress')]
+            ])
+        )
+    
