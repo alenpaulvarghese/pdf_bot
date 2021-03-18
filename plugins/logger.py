9cb8cd15ebe08921f81bcde4b2a2050fbe4c4379
alenpaul2001 <alenpaul2001@gmail.com>
2021-03-18 13:55:21 +0530

    changes :-
    - changed logging level to `INFO`
    - added `force` to override any other basicconfig
    - suppressed logging level of `PIL` to `WARNING`

--- plugins/logger.py
+++ plugins/logger.py
@@ -2,11 +2,13 @@ import logging
 
 
 logging.basicConfig(
-    level="DEBUG",
+    level=logging.INFO,
     format="%(asctime)s - [ %(message)s ]",
     datefmt="%d-%b-%y %H:%M:%S",
+    force=True,
     handlers=[logging.FileHandler("debug.log"), logging.StreamHandler()],
 )
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("PIL").setLevel(logging.WARNING)
 LOG_ = logging.getLogger("core")
