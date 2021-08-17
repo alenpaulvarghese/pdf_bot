41a770bf38269f45ae332cf01d333ec64eb435da
alenpaul2001 <alenpaul2001@gmail.com>
2021-08-17 21:52:10 +0530

    removed obsolute code

--- tools/scaffold/__init__.py
+++ tools/scaffold/__init__.py
@@ -26,24 +26,3 @@ class AbstractTask(object):
 
     def cleanup(self) -> None:
         shutil.rmtree(self.cwd)
-
-
-# class PdfTask2(GeneralTask):
-#     def __init__(self, client: Client = None, chat_id: int = 0, message_id: int = 0):
-#         # input file send from client side
-#         self.input_file = ""
-#         # user input which includes password or page range
-#         self.user_input = ""
-
-#         super().__init__(client, chat_id, message_id)
-
-#     def __del__(self):
-#         for each_file in self.input_file, self.output:
-#             if os.path.exists(each_file):
-#                 os.remove(each_file)
-
-#     async def allocate_and_download(self, message: Message):
-#         await self.file_allocator()
-#         self.input_file = await message.download(
-#             self.cwd + str(self.message_id) + ".pdf"
-#         )
