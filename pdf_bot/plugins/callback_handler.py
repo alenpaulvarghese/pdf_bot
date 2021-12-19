6e06b5587298dfd02eecd2ea3964677692ddc7bb
alenpaul2001 <alenpaul2001@gmail.com>
2021-12-19 10:50:21 +0530

    removed obsolete code

--- pdf_bot/plugins/callback_handler.py
+++ pdf_bot/plugins/callback_handler.py
@@ -61,28 +61,3 @@ async def callback_handler(client: Pdfbot, callback: CallbackQuery):
     elif "remove" in callback.data:
         current_task.temp_files.pop(file_id)
         await callback.message.delete()
-
-
-# @Pdfbot.on_message(filters.command(["extract"]) & filters.create(_task_checker))
-# async def extract_handler(client: Pdfbot, message: Message) -> None:
-#     new_task = Extractor(
-#         client, message.chat.id, message.message_id, " ".join(message.command[1:])
-#     )
-#     client.task_pool.add_task(message.chat.id, new_task)
-#     if not await new_task.parse_input():
-#         return
-#     status = await message.reply_text("**downloading...**")
-#     await new_task.allocate_and_download(message.reply_to_message)
-#     await status.edit(
-#         "**processing...**",
-#     )
-#     client.process_pool.new_task(new_task)
-#     while new_task.status == 0:
-#         await asyncio.sleep(1.2)
-#     else:
-#         if new_task.status == 2:
-#             await message.reply_text(f"**Task failed: `{new_task.error_code}`**")
-#         elif new_task.status == 1:
-#             await message.reply_document(new_task.output)
-#             new_task.__del__()
-#     client.task_pool.remove_task(message.chat.id)
